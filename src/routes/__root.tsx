46c10e473e0494c407f7779876f952be45cf41ab
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2025-02-20 11:19:10 +0200

    Removed devtools in prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,25 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { lazy, Suspense } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/sonner';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import { AppSidebar } from '@/modules/sidebar';
 import { getQueryClient } from '@/shared/api/queryClient';
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : lazy(() =>
+      // Lazy load in development
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      }))
+    );
 
 export const Route = createRootRoute({
   component: () => (
@@ -25,7 +36,9 @@ export const Route = createRootRoute({
               <Outlet />
             </main>
           </div>
-          <TanStackRouterDevtools />
+          <Suspense>
+            <TanStackRouterDevtools />
+          </Suspense>
           <Toaster />
         </SidebarProvider>
       </QueryClientProvider>
