c58c1ea5ffbe3dd71e389e42b0ac477dde649af1
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2025-02-26 02:25:09 +0200

    Added accent to applied filter

--- src/shared/ui/table/filter.tsx
+++ src/shared/ui/table/filter.tsx
@@ -1,5 +1,7 @@
 import type { Column } from '@tanstack/react-table';
 
+import { cn } from '@/lib/utils';
+
 import DebouncedInput from '../inputs/DebouncedInput';
 
 type Props<TData, TValue = unknown> = {
@@ -7,12 +9,17 @@ type Props<TData, TValue = unknown> = {
 };
 
 export function DefaultTableFilter<TData, TValue = unknown>({ column }: Props<TData, TValue>) {
+  const value = String(column.getFilterValue() || '');
+
   return (
     <DebouncedInput
-      className="w-full p-2 py-0 h-8 border border-gray-300 rounded text-sm"
+      className={cn(
+        'w-full p-2 py-0 h-8 text-sm',
+        value ? 'bg-primary text-primary-foreground' : ''
+      )}
       placeholder={`${column.columnDef.header}`}
       throttleTimeout={250}
-      value={String(column.getFilterValue() || '')}
+      value={value}
       onChangeThrottled={column.setFilterValue}
     />
   );
